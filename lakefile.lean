d0002565196613d216273ee7694aa76b23c78960
Charles Ruhland <cruhland@alumni.cmu.edu>
2023-01-14 17:54:10 -0800

    Update Lean 4 toolchain to nightly-2023-01-14

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ package «lean4-analysis»
 -- add package configuration options here
 
 def axiomatic_url := "https://github.com/cruhland/lean4-axiomatic.git"
-def axiomatic_rev := "adf1f7f8b77481c4a59adfd99adb555d365359b4"
+def axiomatic_rev := "9ac5527a6b7384dd96ae54efaeeaf38a847fc673"
 
 require «lean4-axiomatic» from
   /- If you need to pick up local changes to this library, uncomment the line
